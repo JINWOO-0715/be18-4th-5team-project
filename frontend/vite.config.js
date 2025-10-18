78bbda87cb25c156d9a505f8f2e93cecf55ff9d4
JINWOO-0715 <55976921+JINWOO-0715@users.noreply.github.com>
2025-10-19 01:47:06 +0900

    Update Vite config for development environment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,16 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-const isProduction = process.env.NODE_ENV === 'dev'
+const isDevelopment = process.env.NODE_ENV === 'development'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    // 프로덕션 환경이 아닐 때만 vueDevTools 활성화
-    isProduction && vueDevTools(), 
-  ].filter(Boolean), // 배열에서 false 값(비활성화된 플러그인) 제거
-  
+    isDevelopment && vueDevTools(),
+  ].filter(Boolean),
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -21,6 +18,5 @@ export default defineConfig({
   },
   define: {
     global: 'window',
-    // 'process.env': {}
   }
 })
