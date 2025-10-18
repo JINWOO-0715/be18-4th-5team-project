7c656d50062dc10549b0db674423816065d7474d
JINWOO-0715 <55976921+JINWOO-0715@users.noreply.github.com>
2025-10-19 01:50:56 +0900

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-const isDevelopment = process.env.NODE_ENV === 'development'
-
 export default defineConfig({
   plugins: [
     vue(),
-    isDevelopment && vueDevTools(),
   ].filter(Boolean),
 
   resolve: {
